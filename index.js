2f8a929a109a002b0b285b32f6cda9ce8d83fc87
Steve Gardiner <stevegardiner26@aol.com>
2018-11-15 21:06:34 -0500

    Data Entry

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ window.onscroll = function() {myFunction()};
 
 // Get the navbar
 var navbar = document.getElementById("nav");
+var navbar2 = document.getElementsByClassName('navm')[0];
+var hamburger = document.getElementsByClassName('glyphicon-menu-hamburger')[0];
 
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
@@ -11,19 +13,24 @@ var sticky = navbar.offsetTop;
 function myFunction() {
     if (window.pageYOffset >= 1) {
         navbar.classList.add("sticky");
+        navbar2.classList.add("sticky");
+        hamburger.classList.add("gsticky");
     } else {
         navbar.classList.remove("sticky");
+        navbar2.classList.remove("sticky");
+        hamburger.classList.remove("gsticky");
     }
 
-    if((window.pageYOffset >= 700)&&(window.pageYOffset < 2100)){
+    if((window.pageYOffset >= 700)&&(window.pageYOffset < 2200)){
         document.getElementsByClassName('backgroundPic')[0].style.backgroundImage = "url(img/boardwalk.jpg)";
         document.getElementsByClassName('backgroundPic')[0].style.height = "100vh";
         document.getElementsByClassName('backgroundPic')[0].style.filter = "blur(0px)";
     }
-    else if(window.pageYOffset >= 2100){
+    else if(window.pageYOffset >= 2200){
         document.getElementsByClassName('backgroundPic')[0].style.backgroundImage = "url(img/background.jpg)";
         document.getElementsByClassName('backgroundPic')[0].style.filter = "blur(20px)";
         document.getElementsByClassName('backgroundPic')[0].style.height = "100vh";
+        document.getElementsByClassName('backgroundPic')[0].style.height = "700px";
     }
     else {
         document.getElementsByClassName('backgroundPic')[0].style.backgroundImage = "url(img/background.jpg)";
@@ -33,11 +40,44 @@ function myFunction() {
 }
 
 window.onload = function() {
-  document.getElementsByClassName('titleB')[0].style.opacity = '1';
-  document.getElementsByClassName('titleB')[1].style.opacity = '1';
-  document.getElementsByClassName('titleH')[0].style.top = '0';
+    document.getElementsByClassName('titleB')[0].style.opacity = '1';
+    document.getElementsByClassName('titleB')[1].style.opacity = '1';
+    document.getElementsByClassName('titleH')[0].style.top = '0';
 };
 
 function scrollo(el) {
     document.getElementById('' + el).scrollIntoView({ behavior: 'smooth', block: 'center' });
+    closeNav();
+}
+var count = 0;
+function toggleDoor() {
+    console.log(count);
+    if(count%2 === 0) {
+        document.getElementsByClassName('showcase')[0].style.backgroundImage = "url('img/vehicleTest.jpg')";
+        document.getElementsByClassName('showcase')[0].style.backgroundSize = "104%";
+        document.getElementsByClassName('showcase')[0].style.backgroundPosition = "-30px -78px";
+        count++;
+    }
+    else {
+        document.getElementsByClassName('showcase')[0].style.backgroundImage = "url('img/Vehicle2.jpg')";
+        document.getElementsByClassName('showcase')[0].style.backgroundSize = "100%";
+        document.getElementsByClassName('showcase')[0].style.backgroundPosition = "center center";
+        count++;
+    }
+}
+var expanded = false;
+var bar = document.getElementsByClassName('navm')[0];
+function expandNav() {
+    if(expanded === false){
+        bar.classList.add('expandedNav');
+        expanded = true;
+    }
+    else {
+        closeNav();
+    }
+}
+
+function closeNav() {
+    bar.classList.remove('expandedNav');
+    expanded = false;
 }
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!--Designed and Built By Steven Gardiner 9/6/2018 -->
     <meta charset="UTF-8">
     <title>Coastal Kombi</title>
     <link rel="shortcut icon" type="image/x-icon" href="img/kombi.ico" />
@@ -20,11 +21,22 @@
         <div class="insta" onclick="window.open('https://www.instagram.com/njCoastalKombi/', '_blank')"></div>
     </div>
 </nav>
-<div class="titleSlide" id="titles">
+<nav class="navm">
+    <a href="javascript:void(0);" class="icon" onclick="expandNav()" style="float: left;">
+        <span class="glyphicon glyphicon-menu-hamburger"></span>
+    </a>
+    <div class="act2 home" style="margin-top: 55px;" onclick="scrollo('titles')">Home</div>
+    <div class="act2 historyNav" onclick="scrollo('history')">History of the Kombi</div>
+    <div class="act2 contactNav" onclick="scrollo('contact')">Contact Us</div>
+    <div class="act2 socials">
+        <div class="insta" onclick="window.open('https://www.instagram.com/njCoastalKombi/', '_blank')"></div>
+    </div>
+</nav>
+<div class="titleSlide row" id="titles">
     <div class="container">
         <h1 class="titleH">Coastal Kombi</h1>
         <p class="titleB">VolksWagen Bus Rental</p>
-        <div class="row titleB" style="margin: 20px auto; width: 500px;">
+        <div class="row titleB">
             <div class="col-md-6">
                 <button class="btn btn1" style="background: #00CAF0; color: white;" onclick="scrollo('virtual')">Virtual Tour</button>
             </div>
@@ -34,35 +46,103 @@
         </div>
     </div>
 </div>
-<div class="virtualSlide" id="virtual">
-    <div class="row">
-        <h1 class="text-center">Virtual Tour</h1>
-    </div>
-</div>
-<div class="jerseyShore">
-    <div class="row" style="padding-top: 70px;">
-        <h1 class="text-center" style="font-size: 50px; color: white; text-shadow: 0 0 5px rgba(0,0,0,.8)">THE COASTAL KOMBI EXPERIENCE</h1>
-    </div>
-    <div class="row">
-        <p>For Hire ... <br>
-
-            • A 1977, 10-seat Brazilian import Volkswagen "Kombi" Transporter 2 taxi. <br>
-
-            • Available for photography, staging, business prop, commercials, weddings, etc. <br>
-
-            • Hippie bus rental soon to be available as an "Airbnb Experience."</p>
-    </div>
-</div>
-<div class="historyContainer" id="history">
-    <h1 class="text-center" style="color: white; padding-top: 50px;">History of the Kombi</h1>
-</div>
-<div class="contact" id="contact">
-    <div class="container">
-        <div class="col-md-6">
-            <h1 style="color: #333;">Contact Us:</h1>
+<div class="virtualSlide row" id="virtual">
+    <div class="col-md-12">
+        <div class="row">
+            <h1 class="text-center">Virtual Tour</h1>
         </div>
-        <div class="col-md-6">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1 showcase">
+                <div class="door" onclick="toggleDoor()"></div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="jerseyShore row">
+    <div class="col-md-12">
+        <div class="row h1r">
+            <h1 class="text-center" style="color: white; text-shadow: 0 0 5px rgba(0,0,0,.8)">THE COASTAL KOMBI EXPERIENCE</h1>
+        </div>
+        <div class="row">
+            <p><span class="emphasis">For Hire ...</span> <br>
 
+                • A 1977, 10-seat Brazilian import Volkswagen "Kombi" Transporter 2 taxi. <br>
+
+                • Available for photography, staging, business prop, commercials, weddings, etc.<br>
+
+                • Hippie bus rental soon to be available as an "Airbnb Experience."</p>
+        </div>
+    </div>
+</div>
+<div class="historyContainer row" id="history">
+    <div class="col-md-12">
+        <div class="row gifHis">
+            <div class="col-md-6 col-md-offset-3" style="background: #000; height:300px;">
+                <h1 class="text-center" style="color: white;">History of the Kombi <br> (Put animated gif here)</h1>
+            </div>
+        </div>
+        <div class="row textHis">
+            <div class="col-md-5 col-md-offset-1 histScroll">
+                <h3>From the Brazil to New Jersey ...</h3>
+                <p>
+                    "Kombi" originates from the German word Kombinationskraftwagen (combination motor vehicle),
+                    with side windows and removable rear seats, both a passenger and a cargo vehicle combined.
+                    This  Kombi also fits into the Lotação (share-taxi) class of VW Buses, a version exclusive
+                    to Brazil, with 6 front-hinged doors for the passenger area and 4 bench-seats, catering to
+                    the supplemental public transport segment. Available from 1960 to 1989, in both the split-window
+                    and "clipper" (fitted with the bay-window front panel) body styles.
+                </p>
+                <p style="margin-top: 24px;">
+                    This rare version of the 1977 Volkswagon "Kombi" Transporter was only manufactured for one year in a
+                    Brazilian VW factory, and was a special order model for taxi services in the São Paulo, Brazil / South
+                    American market. A rare model there and even rarer sight in North America or Europe. There is a lot to
+                    learn from seeing a VW bus built outside of Germany. While there are many differences, fundamentally
+                    underneath they are all the same. The simplistic "box on wheels" that VW built has been used to haul
+                    nearly everything ... especially people!
+                </p>
+            </div>
+            <div class="col-md-5 histScroll hist2">
+                <p style="margin-top: 54px;">
+                    The tooling from the German plants was sent abroad to the other VW factories and used for many years later.
+                    The rear corner windows on this 1977 model had stopped in German production in 1963. It is a very interesting
+                    blend of early and late on this VW Bus! Three doors on both sides make for a very interesting look — especially
+                    with a front bay window model.
+                </p>
+                <p style="margin-top: 34px;">
+                    Brazil contained the last factory in the world that produced the Transporter of Type 2, which ceased production on
+                    December 31, 2013. This marked the end of the era of rear-engine Volkswagens manufacting, which originated in 1935
+                    with their Type 1 prototypes.
+                </p>
+                <p style="margin-top: 34px;">
+                    Our "Coastal Kombi" was brought stateside from the mountains of São Paulo, Brazil, via Advance Auto Group Car Collectors of Miami, Florida.
+                </p>
+                <h4 class="text-center" style="margin-top: 30px;">
+                    1.600 cc - 1.6 liters <strong>|</strong> 4 speed – Manual <strong>|</strong> Air-Cooled <strong>|</strong> 60 hp
+                </h4>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="contact row" id="contact">
+    <div class="col-md-12">
+        <div class="container">
+            <h1 style="color: #333;" class="text-center">Contact The Coastal Kombi</h1>
+            <div class="col-md-6">
+                <form method="POST" action="contact.php">
+                    <input name="name" type="text" class="form-control" placeholder="Name" required>
+                    <input name="email" type="email" class="form-control" placeholder="Email" required>
+                    <textarea name="message" id="" cols="30" rows="10" class="form-control" placeholder="Message" required></textarea>
+                    <input type="submit" name="submit" class="btn" style="background:#00C1E5;font-size: 24px;height: 36px;width:100%;">
+                </form>
+            </div>
+            <div class="col-md-6 cSocials">
+                <div class="row text-center" style="margin-top: 20px;">
+                    Instagram: <a href="https://www.instagram.com/njCoastalKombi/" target="_blank">@njcoastalkombi</a> <br>-- Middletown, New Jersey
+                </div>
+                <div class="row text-center" style="margin-top: 20px;">
+                    AirBnb Experience: <a href="#">@Coming Soon</a> <br>-- Monmouth County, New Jersey
+                </div>
+            </div>
         </div>
     </div>
 </div>
